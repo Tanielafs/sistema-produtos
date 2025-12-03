73717b643da4e263b64d2f6c789a0a80ac003e58
Taniela Freitas <taniela_santos@yahoo.com>
2025-12-03 10:14:40 -0300

    Add files via upload

---
+++ mock.js
@@ -0,0 +1,5 @@
+const produtos = [
+    { id: 1, nome: "Camiseta", descricao: "Camiseta confortável", preco: "49,90" },
+    { id: 2, nome: "Tênis", descricao: "Tênis esportivo", preco: "199,00" },
+    { id: 3, nome: "Boné", descricao: "Boné estiloso", preco: "39,90" }
+];
